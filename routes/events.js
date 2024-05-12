b4b1e61ae6a3329c616355d019086a10dd540ce7
Martin Ezequiel Williner <williner.dev@gmail.com>
2024-05-12 20:25:13 -0300

    CRUD Events ready

--- routes/events.js
+++ routes/events.js
@@ -2,6 +2,8 @@ const { Router } = require('express');
 const { check } = require('express-validator');
 const { getEventos, crearEvento, actualizarEvento, eliminarEvento } = require('../controllers/events');
 const {validarJWT} = require('../middlewares/jwtValidate');
+const { inputsValidator } = require('../middlewares/inputsValidate');
+const { isDate } = require('../helpers/isDate');
 
 const router = Router();
 
@@ -12,7 +14,13 @@ router.use(validarJWT); // con esta linea, todo lo de abajo debe tener el jwt
 router.get('/', getEventos);
 
 //crear un nuevo evento
-router.post('/', crearEvento);
+router.post('/', 
+[
+    check('title', 'El titulo es obligatorio').not().isEmpty(),
+    check('start', 'Fecha de inicio es obligatoria').custom(isDate),
+    check('end', 'Fecha de finalización es obligatoria').custom(isDate),   
+    inputsValidator
+],crearEvento);
 
 //actualizar evento
 router.put('/:id', actualizarEvento);
